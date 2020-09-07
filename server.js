525cee237b8787fa8ff9171a2c4ebce7d6dc8e51
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-09-07 13:12:47 -0300

    conectando ao DB com o mongoose

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express  = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando a App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", { useMongoClient: true }).then(()=>{
+    console.log("MongoDb Conectado ...")
+}).catch((err)=>{
+    console.log("Houve um erro ao se conectar ao mongoDB: " + err)
+});
+
+//Primeira rota
 app.get('/', (req, res)=>{
     res.send('hello world! com nodemon')
 });
