0d0be3a24e142d41caf789c80161bd96c32d5e8a
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-09-07 18:03:06 -0300

    criado metodo update e delete para atualização e exclusão dos produtos

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,10 +9,28 @@ module.exports = {
         return res.json(products);
     },
 
+    async show(req, res){
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
     async store(req, res){
         //Criação do produto
         const produt = await Product.create(req.body);
 
         return res.json(produt);
+    },
+
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        
+        return res.json(product);
+    },
+
+    async destroy(req, res){
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
     }
 };
