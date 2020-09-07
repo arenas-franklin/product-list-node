8dec517477c701378e491b67274d1e995b899b51
Franklin Angelo <franklin.silva8@fatec.sp.gov.br>
2020-09-07 17:27:58 -0300

    criado metodo post para criação de produtos

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const requireDir = require('require-dir');
 
 // Iniciando a App
 const app = express();
+app.use(express.json());
 
 //Iniciando o DB
 mongoose.connect("mongodb://localhost:27017/nodeapi", {  'useUnifiedTopology': true,  'useNewUrlParser': true }).then(()=>{
@@ -14,17 +15,8 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {  'useUnifiedTopology': t
 
 requireDir("./src/models");
 
-const Product = mongoose.model('Product');
 
-//Primeira rota
-app.get('/', (req, res)=>{
-    Product.create({
-        title: 'React native',
-        description: 'Biuld native aps width react',
-        url: 'https://github.com/facebook/react-native'
-    });
-
-    return res.send('hello world! com nodemon')
-});
+//Rotas
+app.use('/api', require("./src/routes"));
 
 app.listen(3001);
